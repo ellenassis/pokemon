d55d546a07da6de866dd8afefb4f789f0c5bf235
ellenassis <ellenassis8@gmail.com>
2023-02-10 14:26:04 -0300

    feat: add functionality to PokemonList

---
+++ src/components/PokemonList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="">
+    <div v-for="(pokemon) in pokemonAndEvolutions" :key="pokemon.id">
+      <p>{{ pokemon.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { usePokemonStore } from "@/stores/pokemon";
+import { mapState } from "pinia";
+
+export default defineComponent({
+  methods: {},
+  computed: {
+	...mapState(usePokemonStore, ['pokemonAndEvolutions']),
+  }
+});
+</script>
