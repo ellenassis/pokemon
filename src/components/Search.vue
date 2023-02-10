3c6aad4688b28ab218123a7031cf0f668b152e5f
ellenassis <ellenassis8@gmail.com>
2023-02-10 11:21:55 -0300

    feat: add search component

---
+++ src/components/Search.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="search-container">
+    <input type="search" placeholder="Digite o nome do pokémon desejado...">
+    <button><fa icon="search" /></button>
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import { createApp } from "vue";
+
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
@@ -6,9 +7,16 @@ import router from "./router";
 
 import "./assets/css/style.css";
 
+
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+library.add(fas);
+
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
+app.component('fa', FontAwesomeIcon)
 app.mount("#app");
