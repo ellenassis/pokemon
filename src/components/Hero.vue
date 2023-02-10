97ccb35b8a06245eb4d9a5e5b0fe817cebc07aac
ellenassis <ellenassis8@gmail.com>
2023-02-09 23:10:22 -0300

    feat: add hero component

---
+++ src/components/Hero.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="hero-container">
+    <div>
+      <h1>Pokémon</h1>
+      <p>Veja informações sobre os pokémons e suas evoluções.</p>
+    </div>
+    <img src="../assets/images/squirtle.png" alt="pokemon squirtle" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+</script>
