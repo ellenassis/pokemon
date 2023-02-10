803af911cd4cbdd130c9c38db3f3728c236852fd
ellenassis <ellenassis8@gmail.com>
2023-02-10 14:23:06 -0300

    feat: add functionality to SearchPokemon

--- src/components/Search.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="search-container">
-    <input type="search" placeholder="Digite o nome do pokémon desejado...">
-    <button><fa icon="search" /></button>
-  </div>
-</template>
-
-<script lang="ts" setup>
-</script>
---
+++ src/components/SearchPokemon.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="search-container">
+    <input v-model="query" type="search" placeholder="Digite o nome do pokémon desejado..." />
+    <button @click="searchPokemon"><fa icon="search" /></button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { usePokemonStore } from "@/stores/pokemon";
+
+export default defineComponent ({
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    searchPokemon() {
+      const pokemonStore = usePokemonStore();
+      pokemonStore.getPokemon(this.query);
+    },
+  },
+});
+</script>
