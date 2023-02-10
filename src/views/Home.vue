a4ccf61e7bb85c74aecaf35be2d09c25a320d965
ellenassis <ellenassis8@gmail.com>
2023-02-10 14:27:19 -0300

    add components to Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,15 @@
 <template>
-  <main>
-    <h1>Pokémon</h1>
-  </main>
+  <div>
+    <main class="main-container">
+      <Hero />
+      <SearchPokemon />
+      <PokemonList />
+    </main>
+  </div>
 </template>
 
 <script setup lang="ts">
-
+import Hero from "@/components/Hero.vue";
+import SearchPokemon from "@/components/SearchPokemon.vue";
+import PokemonList from "@/components/PokemonList.vue";
 </script>
