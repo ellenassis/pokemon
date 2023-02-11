45ad118931e300943d249e89db09e2d3a2b87622
ellenassis <ellenassis8@gmail.com>
2023-02-11 01:05:52 -0300

    style: hero component

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,11 +1,6 @@
 <template>
   <div class="hero-container">
-    <!-- <div>
-      <h1>Pokémon</h1>
-      <p>Veja informações sobre os pokémons e suas evoluções.</p>
-    </div> -->
     <img src="../assets/images/pikachu-gif.gif" alt="pokemon squirtle" />
-
   </div>
 </template>
 
