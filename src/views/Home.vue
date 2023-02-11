ff1a70bd71313102b66767f0ca5ffeb2af014fd3
ellenassis <ellenassis8@gmail.com>
2023-02-11 01:07:36 -0300

    fix: Home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,8 +8,18 @@
   </div>
 </template>
 
-<script setup lang="ts">
+<script lang="ts">
 import Hero from "@/components/Hero.vue";
 import SearchPokemon from "@/components/SearchPokemon.vue";
 import PokemonList from "@/components/PokemonList.vue";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    Hero,
+    SearchPokemon,
+    PokemonList
+  },
+});
 </script>
